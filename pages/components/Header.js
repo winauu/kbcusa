7c0035d477504d8da66a7fc4f96bf026df674637
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-09-19 17:24:19 -0600

    done with header

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,12 +3,22 @@ import Image from 'next/image'
 
 function Header() {
   return (
-    <div>
-        <Image 
+    <div className='bg-purple-500 text-white flex items-center justify-center space-x-10'>
+        {/* <Image 
             src="/images/header.gif"
             width={1023}
             height={129}
-        />
+        /> */}
+        <div>
+          <Image src='/Removal-844.png' width={140} height={140}/>
+        </div>
+        <div className='text-4xl'>
+          <h1 className='font-sans pb-3'>Central North Area</h1>
+          <p>ခၢၣ်သးကလံၤကဝီၤ</p>
+        </div>
+        <div>
+          <h1 className='font-sans text-6xl text-red-600 shadow'>KBCUSA</h1>
+        </div>
     </div>
   )
 }
