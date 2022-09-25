d95bcae33e624c1bd5972a719f5ea57ff5ac2c09
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-09-24 21:09:18 -0600

    test1

--- pages/components/Churches/FKBCO/FKBCOParagraph1Right.js
+++ pages/components/Churches/FKBCO/FKBCOParagraph1Right.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from &apos;react&apos;
 
 function FKBCOParagraph1Right() {
   return (
     <div>
       <span class="k2">
-      b. w&gt; p; xD.  tDR cD zsd zk o &amp;. 'd. rg nd. whR &lt;A o; yS&gt;&nbsp; og xd eJ. 'D; o &amp;. rk. zD qd jcH; 'D; [H.  zd CD zd t*R u td. 0J A 8 'l. A 9 'l. A&nbsp;  w z. [; bg o ud; ,GR vX ySR [H. w zsX. b. w zsX. 0HR t vD&gt;&nbsp; zJ A 2009 eH. M. vDRIA w&gt; p; xD. 'd; M&gt;  w&gt; cGJ;w &gt; ,m bg b. ,GR vX o &amp;dm wX&nbsp;  uDR vR 0g t o &amp;dm zJ A 2009 eH. vg ,lR vH t ylR&nbsp; zJ w&gt; tD xH csD qX t vD&gt; u td. w vg 0HR  w&gt; p; xD. bg b. ,GR vX o &amp;dm ylR M. vDRIA w&gt; bg ,GR t w&gt; qX  uwD&gt; rh&gt; 0J A12;30e. &amp;H. wkR A 2;00 e. &amp;H. &lt;A rh rh&gt; td.  bSH; eHR w&gt; rR vd b. w&gt; p; xD. tDR zJ A 11 e. &amp;H. M. vDRIA w&gt; td.  zSd. zd [H. 'l. u td. 0J A 60 bsJ. &lt;A 'D; w&gt; td. zSd. u td. 0J A 300bsJ.  M. vDRIA <br/>
+      b. w&gt; p; xD.  tDR cD zsd zk o &amp;. &apos;d. rg nd. whR &lt;A o; yS&gt;&nbsp; og xd eJ. &apos;D; o &amp;. rk. zD qd jcH; &apos;D; [H.  zd CD zd t*R u td. 0J A 8 &apos;l. A 9 &apos;l. A&nbsp;  w z. [; bg o ud; ,GR vX ySR [H. w zsX. b. w zsX. 0HR t vD&gt;&nbsp; zJ A 2009 eH. M. vDRIA w&gt; p; xD. &apos;d; M&gt;  w&gt; cGJ;w &gt; ,m bg b. ,GR vX o &amp;dm wX&nbsp;  uDR vR 0g t o &amp;dm zJ A 2009 eH. vg ,lR vH t ylR&nbsp; zJ w&gt; tD xH csD qX t vD&gt; u td. w vg 0HR  w&gt; p; xD. bg b. ,GR vX o &amp;dm ylR M. vDRIA w&gt; bg ,GR t w&gt; qX  uwD&gt; rh&gt; 0J A12;30e. &amp;H. wkR A 2;00 e. &amp;H. &lt;A rh rh&gt; td.  bSH; eHR w&gt; rR vd b. w&gt; p; xD. tDR zJ A 11 e. &amp;H. M. vDRIA w&gt; td.  zSd. zd [H. &apos;l. u td. 0J A 60 bsJ. &lt;A &apos;D; w&gt; td. zSd. u td. 0J A 300bsJ.  M. vDRIA <br/>
       <br/>
-      <strong>w&gt;  td. zSd. t rD xd.  = pH; xD. y -wXR A 119; 133A 'k; xGJ , cD. vX e u  vk&gt; u xg tylR wuh&gt;IA 'D; rf w&gt; tX w&gt; oD t ok w yX ,R eD w rHR  w*hRIA<br/>
+      <strong>w&gt;  td. zSd. t rD xd.  = pH; xD. y -wXR A 119; 133A &apos;k; xGJ , cD. vX e u  vk&gt; u xg tylR wuh&gt;IA &apos;D; rf w&gt; tX w&gt; oD t ok w yX ,R eD w rHR  w*hRIA<br/>
       </strong><br/>
-      w&gt; td. zSd. t w&gt; bg M. b. w&gt; [H;  M&gt; oud; rl 'g vX w&gt; td. zSd. &lt;A ydm rk. u &amp;X &lt;A usJ; pX;  u&amp;X w EGH w bsD pkm pkm M. vDRIA<br/>
+      w&gt; td. zSd. t w&gt; bg M. b. w&gt; [H;  M&gt; oud; rl &apos;g vX w&gt; td. zSd. &lt;A ydm rk. u &amp;X &lt;A usJ; pX;  u&amp;X w EGH w bsD pkm pkm M. vDRIA<br/>
       </span>
     </div>
   )
