20bb92ae7fe7171d58fddce0813f6c1ca6308275
Win Aung <91712429+winaung1@users.noreply.github.com>
2022-12-28 20:58:44 -0700

    karen

--- pages/components/SecondHomePage/SecondMainContent.js
+++ pages/components/SecondHomePage/SecondMainContent.js
@@ -5,67 +5,88 @@ function SecondMainContent() {
   return (
     <div>
       <Image src='/images/FirstlinkImage/firstLinkImg.jpg' width={1024} height={260} alt='firstLinkImg'/>
-      <div className='min-h-screen bg-white p-2'>
+      <div className='text-2xl min-h-screen bg-white p-2'>
           <div className='w-3/4 mx-auto'>
             <h1 className='text-red-500 text-3xl text-center py-3'>uvHRpd;cX.o;u0DR w{'>'}pH.pdRubsH;yR</h1>
             <hr className='font-bold text-3xl w-3/4 mx-auto border-[1px] border-black mb-3'/>
-            <p>dsfafdsa</p>
-            <p className='indent-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
+            <p>rf,GRvXtrl0Jw*R tw&gt;tJ.w&gt;uGH tw&gt;Cl w&gt;fzd; tw&gt;qd.*hRqd.0g uvDR b. vX ,GR tySR 
+                *D&gt;rk&gt; tvdR xl;xl; wDR wDR &apos;D; <span className='font-sans text-sm'>,</span> Arfc&HmtrHRb.w&gt;pH; 
+                xD.y-wXR tpdRpdRC.C.wuh&gt;e‹.A rh&gt;y w&gt;qX*hRb.okvXc&HmtrHRe‹.vDRItRrh. I</p>
+            <p className='pt-3'>
+                ,GR rRw&gt; &apos;d.vX y0JunDzdwz. y*D&gt;&apos;D; 
+                AAzJ2007eH.tvDcH unDzdwz. [JwkR0JvX uD&gt;trJ&uRtgtg*D&gt;*D&gt; M.vDRI 
+                uDtrJ&u;bsXxHc&Hmzd Aw&gt;&hvdmo; &apos;D; unDzdwz. bl;&apos;D;wH&gt;0JM.vDRI A,GR[h.ySR o; 
+                vXtul.w&gt;oh vXto;td. rRvRuyDR,GR tCd AunDzd wz. &apos;k;td.xD.0J AuD&gt; 
+                trJ& uRunDbsXxHc&Hmzd w&gt;td&gt; zSd. o ud; w&gt;td.zSd.A KBCUSAAvX uD&gt;trJ&uRtylRM.vDRI A 
+                uD&gt; trJ&uRbsXxHc&Hmzd ABC eH. uwX&gt;w&gt;td.zSd. AzJ vg ,lR 25oDwkR26oD <span className='font-sans text-sm'>,</span>2009eH. AvXtvDRb.zJ 
+                <span className='font-sans text-sm'>Los Angeles</span> tcg<span className='font-sans text-sm'>,</span> 
+                AcX. p; vX Au nDbsX xHc &Hm zdw&gt; td.zSd. vX t [Jol. vDR o;zJuD&gt; trJ&uR ylRw z. &apos;D;<span className='font-sans text-sm'>, KBC,TKBC,KKBC,ABC</span>cX.p; 
+                wz.<span className='font-sans text-sm'>,</span> A xH. vdm o;&apos;D; &apos;k; td. xD. 0J (KBCUSA) AyXqSX w&gt; u rH;wHR tqd u wX&gt; 
+                wzkM.vDRI AA KBCUSA w&gt;xH. vdmcH bsD w bsD AzJ th&apos;h,gegzD.0hR tcg Aw&gt; qDvDR 0J&apos;. u0DR EGHu0DR &apos;D; Ayu0DR trHRrh&gt;0J Au0DRcH
+                <span className='font-sans text-sm'>,</span> uvHRpd;cX.o;u0DR <span className='font-sans text-sm'>Central North,</span> 
+                vXty.Ckm <span className='font-sans text-sm'>Colorado, Nebraska</span> uD&gt;pJ.w z.M. vDRI A w&gt;Ck xX xD. Ao&. vX&gt; rl r. wh. &apos;f u0DR cd.w*R tod; 
+                A &apos;D; [h. rl [h.&apos;gtDR vX u&apos;k;td.xD. u0DR tuh&gt; t*DR M.vDRI
             </p>
-            <br/>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
+
+            <p className='pt-3'>
+            u0DRcd. o&. vX&gt; rl r. wh.qJ;usd; vdm o;&apos;D; 
+            u0DRylRw&gt;ftd.zSd. wz. &apos;ftrh&gt; <span className='font-sans text-sm'>(Denver KBC, Omaha FKBC, Lincoln KBC, Sun Set Hills KBC, Alvada KBC, Greeley KBC)</span> 
+            u 0DR w&gt; td. zSd. t cD. xH; wX w bsD b. w&gt; rR tDR zJ , 
+            ElR tg &HR A1 AoD 2010 eH. [gcD 5;00 e.&H. Aw&gt;vD&gt; o&.&apos;d.nl;wh.[H.M.vDRI Au0DR cd. 
+            e&gt; u rH; wHm tqduwX&gt;wzk A rh&gt;0J&apos;.<span className='font-sans text-sm'>,</span> A u 0DR cd.o &.vX&gt;rlr. wh.<span className='font-sans text-sm'>,</span> 
+            u0DR cd.cH o&.&apos;d.rgnl;wh.<span className='font-sans text-sm'>,</span> eJ.&GJ. o&.ed.&apos;h<span className='font-sans text-sm'>,</span> 
+            eJ.&GJ.cH o&.wDwDpd<span className='font-sans text-sm'>,</span> ySRyXR ph o; yS&gt; zdwl; uFL <span className='font-sans text-sm'>,</span> ySRorHo rd; ph o;yS&gt; tg&apos;d.
+            <span className='font-sans text-sm'>,</span> ySR o rH o rd; ph cH o&.uH&apos;,X.<span className='font-sans text-sm'>,</span> ySRuGJ;eD.w&gt; o&.[h&dm&apos;D; o&.&apos;d.pd wz. M.vDRI Au0DR ydmrk.0JR 
+            usdRySR b.rlb.&apos;gwz. rh&gt;0J u&Xcd. o &. rk.zDql.csh<span className='font-sans text-sm'>,</span> eJ.&GJ. zH&SJ z D<span className='font-sans text-sm'>,</span> u0DR o; p&gt; 0JR usdR ySR b. rlb. 
+            &apos;gwz.rh&gt;0J u &Xcd. o&.uH&apos;,X.<span className='font-sans text-sm'>,</span> eJ.&GJ. o&.rk.q. vDRtJ.<span className='font-sans text-sm'>,</span> u 0DR c&Hm zdw&gt; od. vd 0JR usdR ySRb. rlb.&apos;g wz.rh&gt;0J u&X cd. 
+            o&.rk.&apos;g tJ.qd AeJ.&GJ. o&.vgph wz.M.vDRIA u 0DR cd. e&gt; w z. u b. w&gt; Ck xX tDRA 3 A eH. w bsD M. vDRI u 0DR t w&gt; wdm xl tqduwX&gt;  rh&gt; 0J  AxgpH;xD.y -wXRA118;13 ? yurRw&gt;&apos;D; w&gt; *H&gt; ql. bg ql.vX ,GR vDRI/  
+            A&apos;D;xg t oD A278 AM.vDRI  w&gt;CkxXxD.u 0DR t w&gt; wdm xlcDzsdw&gt;bk;rJ&apos;D; w&gt;zD.b.0JvX o &. wD wD pd qSXxD.0JM. vDRIAA <span className='font-sans text-sm'>KBCUSA</span> [h.w&gt;ysJ tCd A zJu0DRw&gt;td.zSd. (5)0Dw0D 
+            <span className='font-sans text-sm'>,</span>Avg, ElR tg &HR A28 oD <span className='font-sans text-sm'>,</span> 2012eH.<span className='font-sans text-sm'>,</span> vXtvDRb.zJ A-uHvH R unDbsXxHc&Hmzd w&gt;td.zSd.<span className='font-sans text-sm'>,</span> 
+            cdRvdR&R'dRAAtcg AunDw&gt;td.zSd.zJArH.pl;&H uD&gt; pJ.A[JEkm rR oud;w&gt; &apos;D; yu 0DRM. vDRI AtCd yu0DR t ylR uD&gt;pJ. vX trR o ud; w&gt;td.0JA 3 Abh. vXtrh&gt; (Colorado, Nebraska, Missouri ) wz. M.vDRI Ayu0DRylR Aytd.&apos;D; 0JRusdR&apos;ftrh&gt; ydmrk. 0JRusdR <span className='font-sans text-sm'>,</span> 
+            o;p&gt; 0JRusdR <span className='font-sans text-sm'>,</span> cIw 0JRusdR A&apos;D; w&gt;o;ck upD.0JR usdRwz. M.vDRI
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
+
+            <p className='pt-3'>
+                chcgqXu wD&gt;cgtHRA u0DRySRb.rlb.&apos;gwz. A u0DRcd.wX o&.&apos;d. vX&gt; rlr. wh. 
+                <span className='font-sans text-sm'>,</span> Au0DRcd.cH o&.uyDRxl<span className='font-sans text-sm'>,</span>
+                AeJ.&GJ.wX o&.ed.&apos;h<span className='font-sans text-sm'>,</span>AeJ.&GJ.cH o&.uGJ&gt;ubDvh <span className='font-sans text-sm'>,</span> 
+                ySRyXRph o;yS&gt;rl&apos;h <span className='font-sans text-sm'>,</span> A ySRo rHord;phwX o&.tg&apos;d. <span className='font-sans text-sm'>,</span>
+                A ySRorHord;phcH o&.tJ.xHx. <span className='font-sans text-sm'>,</span>A ySRuGJ;eD.w&gt; o&.uD&gt; &apos;d.rl<span className='font-sans text-sm'>,</span> 
+                o&.&apos;d. pdw z.M.vDRI u0DRydmrk. u&Xcd.e&gt;u&Xcd.wX o&.rk.zJ0grk&gt;<span className='font-sans text-sm'>,</span>A u&Xcd.cH o&.rk. uyDRxl 
+                <span className='font-sans text-sm'>,</span>A eJ.&GJ.wX o&.rk. q.vDR tJ. zD<span className='font-sans text-sm'>,</span>AeJ.&GJ.cH o&.rk.plpH&gt; 
+                <span className='font-sans text-sm'>,</span> ySRyXRph o&. rk.ypDRzD <span className='font-sans text-sm'>,</span>AySRorHord;phwX o&.rk.vgph 
+                <span className='font-sans text-sm'>,</span>AySRorHord;phcH o&. rk.tJ. rl&R <span className='font-sans text-sm'>,</span> 
+                AySRuGJ;eD.w&gt;wX&apos;D;cH o&.rk.[J0g<span className='font-sans text-sm'>,</span> o&.rk.[Jeh&gt;rl M.vDRIAAA u0DRusJ;pX;u&X0JRusdRcd.e&gt;AAu&Xcd.wX o&. 
+                rk. q.vDRtJ. <span className='font-sans text-sm'>,</span>A u&Xcd.cH o&.tJ.eh&gt;rl <span className='font-sans text-sm'>,</span>AeJ.&GJ.cd.usX&gt; o&.rk. 
+                unDzDpd<span className='font-sans text-sm'>,</span>A eJ.&GJ.oCJR o&. -uh; pd<span className='font-sans text-sm'>,</span>A ySRyXRph o&.qXeh&gt;0g
+                <span className='font-sans text-sm'>,</span>A ySRorHord;ph o&.rk. tJ.uD&gt;ck.<span className='font-sans text-sm'>,</span>eD&gt;tJ.q.xl<span className='font-sans text-sm'>,</span>
+                A ySRuGJ;eD.w&gt; tJ.uvkmzD<span className='font-sans text-sm'>,</span>pDRxdvGh.<span className='font-sans text-sm'>,</span>A ySR[h.ul. [h.z; w&gt; wz.rh&gt; 0J o&.&apos;d. wz.&apos;D; w&gt; td. 
+                zSd. o&. wz.M.vDRIAAA u0DR cIw cd.e&gt;wz.AAu&Xcd.wX o&.0h&gt;rl <span className='font-sans text-sm'>,</span>Au&Xcd.cH o&.rk.zDv. <span className='font-sans text-sm'>,</span>A eJ.&GJ. wX o&.q. tJ.0g  
+                <span className='font-sans text-sm'>,</span>AeJ.&GJ.cH o&.rk.pH; <span className='font-sans text-sm'>,</span>  ySRyXRph  <span className='font-sans text-sm'>,</span> o&.rk.rlzDv. <span className='font-sans text-sm'>,</span> 
+                ySRorHord;phwX o&.rgbvX;pf <span className='font-sans text-sm'>,</span> AySRorHo rd; ph cH o&. rk.puHxX. <span className='font-sans text-sm'>,</span> A ySRuGJ;eD.w&gt; o&.tgpJ;rl <span className='font-sans text-sm'>,</span> 
+                A&apos;D; urH;wHR&apos;D;ySR[h.ul. w&gt;wz. rh&gt;0JR Ao&.rk.tJ.z<span className='font-sans text-sm'>,</span> Ao&.rk.cFl;bJ <span className='font-sans text-sm'>,</span> Ao&.rk.ueDR <span className='font-sans text-sm'>,</span>  
+                zHzHo&.rk. uFJ&h.vhM.vDRIAA u0DR w&gt;o;ckupD.0JRusdR cd.e&gt;wz.ySRyXRvD&gt;qh.eDR o&.&apos;d.uGJRxl <span className='font-sans text-sm'>,</span> AySReJ.w&gt; o&.eDrJ pdbd. ySRyXRph o&.phRuFX.pXRySRorHord;ph o&.ol;oh.cD.wz.M.vDRI
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
+
+            <p className='pt-3'>
+            uD&gt;pJ. oXbh.tylRw&gt; td. zSd. rRoud;w&gt;td. 0J A(7 )AzSd. &apos;D; Aw&gt;td.zSd.zd td.0J 1289 *RM. vDRI Ay u 0DR ylR o &. &apos;d. td. A(4 )A*R vX t rh&gt; o &. &apos;d. uGJR xl &apos;D; o &. &apos;d. rg nd. wh. <span className='font-sans text-sm'>,</span> o&.&apos;d.vX&gt;rlr.wh. &apos;D; o&.rk.&apos;d.&apos;bd&Roh. w z. M. vDRI Au 0DR w&gt; td. zSd. w&gt; xH. vdm td. 0J w eH. A1 AbsD &apos;D; vDwJpd w&gt;td.zSd. td. 0J&apos;fw&gt;vd.b.tod;M.vDRI u0DRw&gt;[l;w&gt;*JRwz. w&gt;[;td.oud;w&gt;td.zSd.wz.<span className='font-sans text-sm'>,</span> &apos;D;[h.cd.e&gt;w&gt;rRvdwz. &apos;D; td.&apos;D;w&gt;[l; w&gt;*JRvDRqD zJ u0DReH.uwX&gt;w&gt; td.zSd. M.vDRI A
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
+            <p className='pt-3'>yu0DRw&gt;td.zSd.wz.rh&gt;0J =</p>
+            <ol className=''>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>1I&apos;hbX&gt;unDbsXxHc&Hmzdw&gt;td.zSd.<span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&.=o&.'d.vX&gt;rl r.wh.</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>2I thRbhehp&gt;unDbsXxHc&Hmzdw&gt;td.zSd.<span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&.= o&.rk.&apos;d.&apos;bd&R</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>3I Ao&dmwXunDbsXxHc&Hmzdw&gt;td.zSd.td.r[.<span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&. = o&.&apos;d.rgnd.wh.</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>4I td.r[.unDbsXxHc&Hmzdw&gt;td.zSd. <span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&. = o&.&apos;d.uGJRxl</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>5I xHcX&gt;eHRunDbsXxHc&Hmzdw&gt;td.zSd. <span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&. = o&.uyDRxl</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>6I vhcX&gt;unDbsXxHc&Hmzdw&gt;td.zSd.<span className='font-sans text-sm'>,</span></a>Aw&gt;td.zSd.o&.=o&.tgpJ;rl</li>
+                <li><a className='underline hover:text-blue-500 hover:cursor-pointer'>7I b.qd.*hRunDbsXxHc&Hmzdw&gt;td.zSd.A <span className='font-sans text-sm'>Missouri,</span></a>Aw&gt;td.zSd.o&.=o&.rk.jyHxl</li>
+            </ol>
+
+            <p className='pt-3'>
+            &apos;Dyk&gt;0J&gt;o;bd vXc&HmtylRcJvXmoh.okဧX AvXy0JunD zdwz. t*D&gt; A,GRtd; xD.usJvX yurRoud;w&gt;vX tbDrk&gt;w&gt;o;ckupD.u&apos;d.xD.t*D &gt; &apos;fySRpl&gt;uhRemuhR w&gt; w z. t od ; A yu*JRvd mCl Cl A yu[H; xD.zD. xD.oud; rl&apos;gClCl vX up&gt;c&HmtbDrk&gt;w&gt;o;ckupD.*D&gt; wkRvX w&gt;vXw&gt;ySJR wpkM&gt; rh&gt;yw&gt; qX*hR qX0g&apos;D;rh&gt;yw&gt;xkuz.M.vDRI
             </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam officia 
-                totam nihil provident. Voluptatem mollitia tenetur, quis ex maxime pariatur deleniti aperiam minus 
-                alias consequuntur, assumenda facere molestias atque in velit facilis natus vitae! Velit culpa sint, 
-                sapiente temporibus repudiandae error odit repellat fuga maxime minima voluptatum? Qui, hic excepturi?
-            </p>
+
+            <a href="" className='pt-5 font-bold text-3xl underline hover:text-blue-500'>uvHRpd;cX.o;u0DR w&gt;uGJ;eD.wz.</a>
           </div>
       </div>
     </div>
--- pages/components/ThirdHomePage/ThirdMainContent.js
+++ pages/components/ThirdHomePage/ThirdMainContent.js
@@ -5,8 +5,8 @@ function ThirdMainContent() {
   return (
     <div>
       <div className='flex flex-wrap w-3/4 p-2 gap-5 mx-auto'>
-        <HomePagePastorsImg img='/images/ThirdLinkImages/KweKbawLay.jpg' definition='loremh akija hdslo aoiu ahd' alt='KweKbawLay'/>
-        <HomePagePastorsImg img='/images/ThirdLinkImages/Nuyttin.jpg' definition='loremh akija hdslo aoiu ahd' alt='Nuyttin'/>
+        <HomePagePastorsImg img='/images/ThirdLinkImages/KweKbawLay.jpg' definition='o&.&apos;d.uGJ&gt;ubDvh u. u0DRcd.' alt='KweKbawLay'/>
+        <HomePagePastorsImg img='/images/ThirdLinkImages/Nuyttin.jpg' definition='o&.&apos;d.rgnd.wh.Au0DRcd.cH' alt='Nuyttin'/>
         <HomePagePastorsImg img='/images/ThirdLinkImages/panawdee.jpg' definition='loremh akija hdslo aoiu ahd' alt='panawdee'/>
         <HomePagePastorsImg img='/images/ThirdLinkImages/Isaac.jpg' definition='loremh akija hdslo aoiu ahd' alt='Isaac'/>
         <HomePagePastorsImg img='/images/ThirdLinkImages/MooDay.jpg' definition='loremh akija hdslo aoiu ahd' alt='Mooday'/>
